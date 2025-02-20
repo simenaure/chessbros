f28b60364b667c2dc7d08bbcb8b4c79ad5d5bf36
Aleksander <aleks.flesen@gmail.com>
2025-02-20 15:22:06 +0100

    Transfer to home commit

--- chess-bro/src/main.tsx
+++ chess-bro/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Profile from './profile/profile.tsx';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <nav><Link to = "/profile">Go to profile page</Link></nav>
+    </BrowserRouter>
   </StrictMode>,
 )
