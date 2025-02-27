8cd8683c2078e004bff41c126cf0c5fc9d9d6231
Jens <jensops@stud.ntnu.no>
2025-02-27 10:29:01 +0100

    Database

--- chess-bro/src/main.tsx
+++ chess-bro/src/main.tsx
@@ -1,42 +1,42 @@
-import './index.css'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import LoginSetup from './login/logintosignup.tsx'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import FrontPage from './pages/FrontPage.tsx';
-import ProfilePage from './pages/ProfilePage.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import Layout from './pages/Layout.tsx';
-import Users from "./pages/DatabasePage.tsx";
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import LoginSetup from "./login/logintosignup.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import FrontPage from "./pages/FrontPage.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import Layout from "./pages/Layout.tsx";
+import Users from "./pages/databasepage.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <FrontPage />,
       },
       {
-        path: '/profile',
-        element: <ProfilePage />
+        path: "/profile",
+        element: <ProfilePage />,
       },
       {
-        path: '/login',
-        element: <LoginSetup />
+        path: "/login",
+        element: <LoginSetup />,
       },
       {
-        path: '/users',
-        element: <Users />
-      }
+        path: "/users",
+        element: <Users />,
+      },
     ],
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>
-)
+);
