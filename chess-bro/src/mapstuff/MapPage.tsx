e5792e97fdc64b8e8bea3ec6d9e9e7ecc421cef0
Simen Aure Skogedal <simeasko@stud.ntnu.no>
2025-02-27 11:03:34 +0100

    kart på plass

--- chess-bro/src/mapstuff/MapPage.tsx
+++ chess-bro/src/mapstuff/MapPage.tsx
@@ -1,5 +1,69 @@
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+/*import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"*/
+import { useEffect } from "react";
+
+
+
+
+/*
 export default function MapPage() {
-   return(111)
-  }
+   
+    /*const L = require('leaflet');*/
+   /* var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+ 
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  
+  return(
+
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossOrigin=""
+        />
+         <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+       integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+       crossOrigin=""></script>
+  
+        <div id="map"></div>
+      </div>
+    )
+
+
+}*/
+
+
+
+export default function MapPage() {
+  useEffect(() => {
+    // Ensure the map is only initialized once
+    if (!document.getElementById("map")?.hasChildNodes()) {
+      var map = L.map("map").setView([63.43, 10.4], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(map);
+    }
+  }, []);
+
+  return (
+   
+    
+      <div id="map" 
+      style={{ height: "700px", width: "100%" }}
+      ></div>
+   
+  );
+}
+
   
   
