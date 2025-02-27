1d27577fc3f2cd17acad1b13842ed142028d524c
Magnus <maglun@stud.ntnu.no>
2025-02-27 09:42:38 +0100

    Save changes before delete and redownload

--- chess-bro/src/main.tsx
+++ chess-bro/src/main.tsx
@@ -1,39 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import LoginSetup from './login/logintosignup.tsx'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom';
-import FrontPage from './pages/FrontPage.tsx';
-import ProfilePage from './pages/ProfilePage.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import Layout from './pages/Layout.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import LoginSetup from "./login/logintosignup.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import FrontPage from "./Front_page/Front.tsx";
+import ProfilePage from "./pages/ProfilePage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import Layout from "./pages/Layout.tsx";
 import Users from "./pages/databasepage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Layout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <FrontPage />,
       },
       {
-        path: '/profile',
-        element: <ProfilePage />
+        path: "/profile",
+        element: <ProfilePage />,
       },
       {
-        path: '/login',
-        element: <LoginSetup />
-      }
+        path: "/login",
+        element: <LoginSetup />,
+      },
     ],
-    errorElement: <ErrorPage />
-  }
+    errorElement: <ErrorPage />,
+  },
 ]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Users />
     <RouterProvider router={router} />
   </StrictMode>
 );
